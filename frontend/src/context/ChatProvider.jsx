58e1d8f3c54a40878de5828cccd765d5302cb299
Khushal Mali <khushalmali8448@gmail.com>
2024-06-26 11:02:48 +0530

    fix

--- frontend/src/context/ChatProvider.jsx
+++ frontend/src/context/ChatProvider.jsx
@@ -11,10 +11,7 @@ const ChatProvider = ({ children }) => {
     const userInfo = JSON.parse(localStorage.getItem("userInfo"));
     setUser(userInfo);
 
-    if (!userInfo) {
-      history.push("/");
-    } else {
-    }
+    if (!userInfo) history.push("/");
   }, [history]);
 
   return (
